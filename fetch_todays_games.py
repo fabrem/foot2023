5b91786402689466af02c7f103bd50466ffc1f4a
fabrem <remilevesque17@gmail.com>
2023-01-30 07:42:55 -0500

    sort by date

--- fetch_todays_games.py
+++ fetch_todays_games.py
@@ -1,5 +1,6 @@
 import json
 import requests
+from datetime import datetime
 
 def fetch_todays_game():
     res = requests.get('http://site.api.espn.com/apis/site/v2/sports/football/nfl/scoreboard')
@@ -38,28 +39,32 @@ def fetch_todays_game_charlem():
     first_team_score_path = ['competitions', 0, 'competitors', 0]
     second_team_score_path = ['competitions', 0, 'competitors', 1]
     events = response[path_to_games]
-    game_name = []
+    game_time = []
     game_scores = []
 
-    for index in range(len(events)):
-        game_name.append(events[index]['name'])
+    # for index in range(len(events)):
 
-    for single_game in events:
+    for index, single_game in enumerate(events):
         first_team_score = single_game
         second_team_score = single_game
+        game_time = events[index]['date']
 
         for subpath in first_team_score_path:
             first_team_score = first_team_score[subpath]
         for subpath in second_team_score_path:
             second_team_score = second_team_score[subpath]
 
-        game_scores.append(((first_team_score['team']['displayName'], first_team_score['score']), (second_team_score['team']['displayName'], second_team_score['score'])))
+        game_scores.append(((first_team_score['team']['displayName'], first_team_score['score']), (second_team_score['team']['displayName'], second_team_score['score']), game_time))
 
     formatted_matches = []
     for match in game_scores:
-        team1, team2 = match
+        team1, team2, game_time = match
         if int(team1[1]) > int(team2[1]):
-            formatted_matches.append((team1[0].split(" ")[-1].lower(), (int(team1[1]), int(team2[1]))))
+            formatted_matches.append((team1[0].split(" ")[-1].lower(), (int(team1[1]), int(team2[1])), match[-1]))
         else:
-            formatted_matches.append((team2[0].split(" ")[-1].lower(), (int(team2[1]), int(team1[1]))))
-    return formatted_matches
+            formatted_matches.append((team2[0].split(" ")[-1].lower(), (int(team2[1]), int(team1[1])), match[-1]))
+        
+    # sort by key here, match time
+    return sorted(formatted_matches, key=lambda x: datetime.fromisoformat(x[-1]))
+
+print(fetch_todays_game_charlem())
